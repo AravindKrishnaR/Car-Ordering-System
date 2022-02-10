1c8c46e5118f0e3e05ac593f87c55f8deb8993d4
Aravind Radhakrishnan <aravindradhakrishnan@L-FVFG4FB0Q05D.local>
2022-02-10 14:51:02 +0530

    Add updates

--- src/main/java/com/training/springboot/model/Car.java
+++ src/main/java/com/training/springboot/model/Car.java
@@ -1,5 +1,49 @@
 package com.training.springboot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Car {
+
+	@Id
+	private int modelNo;
+	private String carName;
+	private String category;
+
+	public Car() {
+		super();
+	}
+
+	public Car(int modelNo, String carName, String category) {
+		super();
+		this.modelNo = modelNo;
+		this.carName = carName;
+		this.category = category;
+	}
+
+	public int getModelNo() {
+		return modelNo;
+	}
+
+	public void setModelNo(int modelNo) {
+		this.modelNo = modelNo;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 }
--- src/main/java/com/training/springboot/model/Orders.java
+++ src/main/java/com/training/springboot/model/Orders.java
@@ -1,5 +1,51 @@
 package com.training.springboot.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Orders {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int orderId;
+	private String username;
+	private int modelNo;
+
+	public Orders() {
+		super();
+	}
+
+	public Orders(int orderId, String username, int modelNo) {
+		super();
+		this.orderId = orderId;
+		this.username = username;
+		this.modelNo = modelNo;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getModelNo() {
+		return modelNo;
+	}
+
+	public void setModelNo(int modelNo) {
+		this.modelNo = modelNo;
+	}
 }
